3ca01bb9653f5a9902311c9e4e3facf3e21e705f
Marcus Hellberg <marcus@vaadin.com>
2018-07-12 12:03:06 -0400

    align code with step 1

--- src/index.js
+++ src/index.js
@@ -2,70 +2,71 @@ import './styles.css';
 const getSpeakers = () => import('./speakers.json');
 const getSchedule = () => import('./schedule.json');
 
-let speakers;
-
-async function init() {
-  if ('IntersectionObserver' in window) {
-    setupNavIntersectionObserver();
+class PWAConfApp {
+  constructor() {
+    this.init();
   }
-  addLoadingIndicatorDelay();
+  async init() {
+    if ('IntersectionObserver' in window) {
+      this.setupNavIntersectionObserver();
+    }
+    this.addLoadingIndicatorDelay();
 
-  await loadSpeakers();
-  await loadSchedule();
-}
+    await this.loadSpeakers();
+    await this.loadSchedule();
+  }
 
-function addLoadingIndicatorDelay() {
-  // Only show spinner if we're delayed more than 1s
-  setTimeout(() => {
-    Array.from(document.querySelectorAll('.loader')).forEach(loader => {
-      loader.removeAttribute('hidden');
+  addLoadingIndicatorDelay() {
+    // Only show spinner if we're delayed more than 1s
+    setTimeout(() => {
+      Array.from(document.querySelectorAll('.loader')).forEach(loader => {
+        loader.removeAttribute('hidden');
+      });
+    }, 1000);
+  }
+
+  setupNavIntersectionObserver() {
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+    const callback = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          [nav, header].forEach(e => e.classList.remove('fixed'));
+        } else {
+          [nav, header].forEach(e => e.classList.add('fixed'));
+        }
+      });
+    };
+    const observer = new IntersectionObserver(callback, {
+      threshold: [0, 1]
     });
-  }, 1000);
-}
+    observer.observe(header);
+  }
+  async loadSpeakers() {
+    this.speakers = (await getSpeakers()).default;
+    const speakersDiv = document.querySelector('.speakers');
 
-function setupNavIntersectionObserver() {
-  const nav = document.querySelector('nav');
-  const header = document.querySelector('header');
-  const callback = entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        [nav, header].forEach(e => e.classList.remove('fixed'));
-      } else {
-        [nav, header].forEach(e => e.classList.add('fixed'));
-      }
-    });
-  };
-  const observer = new IntersectionObserver(callback, {
-    threshold: [0, 1]
-  });
-  observer.observe(header);
-}
-async function loadSpeakers() {
-  speakers = (await getSpeakers()).default;
-  const speakersDiv = document.querySelector('.speakers');
+    speakersDiv.innerHTML = this.speakers.map(this.toSpeakerBlock).join('\n');
+  }
 
-  speakersDiv.innerHTML = speakers.map(toSpeakerBlock).join('\n');
-}
+  async loadSchedule() {
+    const rawSchedule = (await getSchedule()).default;
+    // Add speaker details to array
+    const schedule = rawSchedule.map(this.addSpeakerDetails, this);
+    const scheduleDiv = document.querySelector('.schedule');
+    scheduleDiv.innerHTML = schedule.map(this.toScheduleBlock).join('\n');
+  }
 
-async function loadSchedule() {
-  const rawSchedule = await getSchedule();
-
-  // Add speaker details to array
-  const schedule = rawSchedule.default.map(addSpeakerDetails, this);
-  const scheduleDiv = document.querySelector('.schedule');
-  scheduleDiv.innerHTML = schedule.map(toScheduleBlock).join('\n');
-}
-
-function toSpeakerBlock(speaker) {
-  return `
+  toSpeakerBlock(speaker) {
+    return `
         <div class="speaker">
           <img src="${speaker.picture}" alt="${speaker.name}">
           <div>${speaker.name}</div>
         </div>`;
-}
+  }
 
-function toScheduleBlock(scheduleItem) {
-  return `
+  toScheduleBlock(scheduleItem) {
+    return `
       <div class="schedule-item ${scheduleItem.category}">
         <div class="title-and-time">
           <div class="time">${scheduleItem.startTime}</div>
@@ -79,15 +80,16 @@ function toScheduleBlock(scheduleItem) {
         <p class="description">${scheduleItem.description}</p>
       </div>
     `;
-}
-
-function addSpeakerDetails(item) {
-  if (item.speakerId) {
-    return Object.assign({}, item, {
-      speaker: speakers.find(s => s.id === item.speakerId)
-    });
   }
-  return Object.assign({}, item);
+
+  addSpeakerDetails(item) {
+    if (item.speakerId) {
+      return Object.assign({}, item, {
+        speaker: this.speakers.find(s => s.id === item.speakerId)
+      });
+    }
+    return Object.assign({}, item);
+  }
 }
 
 async function registerSW() {
@@ -103,6 +105,6 @@ async function registerSW() {
 }
 
 window.addEventListener('load', e => {
-  init();
+  new PWAConfApp();
   registerSW();
 });
