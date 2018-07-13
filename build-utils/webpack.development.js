cd4b0d0f273f9e76514a117fc7858609a11c1bc6
Marcus Hellberg <marcus@vaadin.com>
2018-07-13 11:27:40 -0700

    add source maps for development

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -12,5 +12,6 @@ module.exports = () => ({
   plugins: [
     // Copy empty ServiceWorker so install doesn't blow up
     new CopyWebpackPlugin(['src/sw.js'])
-  ]
+  ],
+  devtool: 'source-map'
 });
