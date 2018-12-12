0641b97101ad99f6fc5347ca7552dc7cffa8c59c
Roman Grinyov <w3lifer@gmail.com>
2018-12-12 14:37:32 +0300

    added [sw.js]

--- app.js
+++ app.js
@@ -100,4 +100,17 @@ class PWAConfApp {
 }
 window.addEventListener('load', e => {
   new PWAConfApp();
+  registerSW();
 });
+
+async function registerSW() { // (1)
+    if ('serviceWorker' in navigator) { // (2)
+        try {
+            await navigator.serviceWorker.register('./sw.js'); // (3)
+        } catch (e) {
+            alert('ServiceWorker registration failed. Sorry about that.'); // (4)
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden'); //(5)
+    }
+}
